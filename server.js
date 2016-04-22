6055846de57cd1e7d7b45570cfe5bce16ad7a7f7
Florent Jaby <florent.jaby@gmail.com>
2016-04-22 16:36:15 +0200

    Add structured logging with bunyan

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const hapi = require('hapi')
 const products = require('./lib/products')
 const server = new hapi.Server()
+const logger = require('./lib/logger')
 
 server.connection({ port: 3000 })
 server.route({
@@ -9,6 +10,11 @@ server.route({
   handler: (request, reply) => {
     reply(products.getProducts())
   }
+})
+
+server.register({
+  register: require('hapi-bunyan'),
+  options: { logger }
 })
 
 module.exports = server
