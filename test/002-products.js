0d70cf2ff9c2e32fe6c8511b54cc41dddeb91e78
Simon Renoult <simon.renoult@gmail.com>
2016-04-22 17:29:09 +0200

    Find product by id

--- test/002-products.js
+++ test/002-products.js
@@ -15,4 +15,23 @@ describe('products', () => {
       expect(products.getProducts()).to.be.a('Promise')
     })
   })
+  describe('getProduct', () => {
+    describe('when the product exist', () => {
+      it('should be returned', (done) => {
+        products.getProduct(1).then((product) => {
+          expect(product).to.exist
+          expect(product.id).to.equal(1)
+          done()
+        }).catch(done)
+      })
+    })
+    describe('when the product does not exsit', () => {
+      it('should return null', (done) => {
+        products.getProduct(42).then((product) => {
+          expect(product).to.be.null
+          done()
+        }).catch(done)
+      })
+    })
+  })
 })
--- test/003-product-id.js
+++ test/003-product-id.js
@@ -1,20 +1,50 @@
 'use strict'
 
 const expect = require('chai').expect
+const sinon = require('sinon')
 const server = require('../')
+const products = require('../lib/products')
 
 describe('GET /products/{product_id}', () => {
-  it('should return 501', (done) => {
-    server.inject('/products/42', (res) => {
-      expect(res.statusCode).to.equal(501)
-      done()
+  before(() => {
+    this.getProductStub = sinon.stub(products, 'getProduct')
+  })
+  after(() => {
+    this.getProductStub.restore()
+  })
+  describe('when product does not exist', () => {
+    before(() => {
+      this.getProductStub.returns(Promise.resolve(null))
+    })
+    it('should return 404', (done) => {
+      server.inject('/products/42', (res) => {
+        expect(res.statusCode).to.equal(404)
+        done()
+      })
+    })
+    it('should contain the appropriate values', (done) => {
+      server.inject('/products/42', (res) => {
+        expect(res.result).to.have.property('statusCode').and.to.equal(404)
+        expect(res.result).to.have.property('error').and.to.equal('Not Found')
+        done()
+      })
     })
   })
-  it('should contain the appropriate values', (done) => {
-    server.inject('/products/42', (res) => {
-      expect(res.result).to.have.property('statusCode').and.to.equal(501)
-      expect(res.result).to.have.property('error').and.to.equal('Not Implemented')
-      done()
+  describe('when product exists', () => {
+    before(() => {
+      this.getProductStub.returns(Promise.resolve({id: 1}))
+    })
+    it('should return 200', (done) => {
+      server.inject('/products/1', (res) => {
+        expect(res.statusCode).to.equal(200)
+        done()
+      })
+    })
+    it('should contain the appropriate values', (done) => {
+      server.inject('/products/1', (res) => {
+        expect(res.result).to.have.property('id').and.to.equal(1)
+        done()
+      })
     })
   })
 })
