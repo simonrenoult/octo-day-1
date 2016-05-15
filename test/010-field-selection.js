a175affb7326b7d6f6ed3db558742b185bc167fc
Simon Renoult <simon.renoult@gmail.com>
2016-05-15 19:05:23 +0200

    feat(products): support partial response

---
+++ test/010-field-selection.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const chai = require('chai')
+const chaiThings = require('chai-things')
+
+const server = require('../server')
+const expect = chai.expect
+
+chai.use(chaiThings)
+
+describe.only('GET /products?fields=id,name', () => {
+  before((done) => {
+    server.inject('/products?api_key=foo&fields=id,name', (response) => {
+      this.response = response
+      this.products = JSON.parse(this.response.payload)
+      done()
+    })
+  })
+  it('should return 200', () => {
+    expect(this.response.statusCode).to.equal(200)
+  })
+  it('should contain an id', () => {
+    expect(this.products).to.all.have.property('id')
+  })
+  it('should contain a name', () => {
+    expect(this.products).to.all.have.property('name')
+  })
+  it('should exclude tags', () => {
+    expect(this.products).not.to.all.have.property('tags')
+  })
+})
